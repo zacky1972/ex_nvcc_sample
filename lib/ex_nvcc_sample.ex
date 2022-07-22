81762700968739308ff3f3d9cf62d70aa42d7bf8
Susumu Yamazaki <zacky1972@gmail.com>
2022-07-23 08:14:14 +0900

    initial commit

---
+++ lib/ex_nvcc_sample.ex
@@ -0,0 +1,18 @@
+defmodule ExNvccSample do
+  @moduledoc """
+  Documentation for `ExNvccSample`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExNvccSample.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_nvcc_sample_test.exs
@@ -0,0 +1,8 @@
+defmodule ExNvccSampleTest do
+  use ExUnit.Case
+  doctest ExNvccSample
+
+  test "greets the world" do
+    assert ExNvccSample.hello() == :world
+  end
+end
