a336c9867c29c593c6cd75b18a16f655bf786a62
Susumu Yamazaki <zacky1972@gmail.com>
2022-07-23 09:40:19 +0900

    implement temporary NIF function test

--- lib/ex_nvcc_sample.ex
+++ lib/ex_nvcc_sample.ex
@@ -1,7 +1,23 @@
 defmodule ExNvccSample do
+  require Logger
+
   @moduledoc """
   A sample program that connects Elixir and `nvcc`.
   """
+
+  @on_load :load_nif
+
+  def load_nif do
+    nif_file = '#{Application.app_dir(:ex_nvcc_sample, "priv/libnif")}'
+
+    case :erlang.load_nif(nif_file, 0) do
+      :ok -> :ok
+      {:error, {:reload, _}} -> :ok
+      {:error, reason} -> Logger.error("Failed to load NIF: #{inspect(reason)}")
+    end
+  end
+
+  def test, do: exit(:nif_not_loaded)
 
   @doc """
   Hello world.
